2bbfa633bfff9e81315e7fc4ea8a3c5999e4197a
Husanboy <husanboy.makhmudov09@gmail.com>
2025-04-01 21:13:51 +0500

    first commit

---
+++ header.blade.php
@@ -0,0 +1,17 @@
+<h1>Hello World</h1>
+<p>This is something for learning</p>
+
+<ul>
+    <li>
+        Birinchi
+    </li>
+    <li>
+        Ikkinchi
+    </li>
+    <li>
+        Uchinchi
+    </li>
+    <li>
+        Uchinchi
+    </li>
+</ul>
---
+++ index.php
@@ -0,0 +1,5 @@
+<h1>Bu main branch dagi o'zgarish</h1>
+
+<p>Yangii code qo'shildi</p>
+
+<p>Post branch dagi o'zgarish</p>
